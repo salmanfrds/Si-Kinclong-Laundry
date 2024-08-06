c3df8ddc96e4417108d1b2ee9e8dc4bb9a14f6df
salmanfrds <salmanfird4@outlook.com>
2024-08-06 21:05:19 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      borderWidth: {
+        '0.5': '0.5px',
+      },
+    },
+  },
+  plugins: [],
+}
