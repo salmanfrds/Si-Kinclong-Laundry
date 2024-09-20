9fd7c29e91ead2b6b62426a7fc64e34110139ec3
salmanfrds <salmanfird4@outlook.com>
2024-09-20 17:10:34 +0800

    update  20

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-feather.replace();
-
 function shownav() {
   const hambutton = document.getElementById("hamburger");
   const navtog = document.getElementById("nav-toggle");
@@ -11,24 +9,8 @@ function shownav() {
   });
 };
 
-function checkWidth(){
-  const width = window.innerWidth;
-  const image = document.getElementById("thirdimage");
-
-  if(width >= 1128 && width < 1280){
-    image.classList.add("hidden");
-  } else if (width > 1780){
-    image.classList.add("hidden");
-  } else {
-    image.classList.remove("hidden");
-  }
-};
-
-window.addEventListener("resize", checkWidth);
-
 document.addEventListener("DOMContentLoaded", function() {
     shownav();
-    checkWidth();
 });
 
 const cardSlider = document.getElementById('card-slider');
@@ -46,23 +28,3 @@ prevBtn.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + 4) % 4;
     cardSlider.style.transform = `translateX(-${currentIndex * 100}%)`;
 });
-
-function gallery(){  
-  const div = document.getElementById("gallery");
-  for (let i = 1; i < 12; i++) {
-    const img = document.createElement("img");
-
-    img.src = "src/gallery/" + i + ".jpg";
-    img.alt = "Gallery image" + i;
-
-    img.className = "w-44 h-44 md:w-80 md:h-80 object-cover rounded-xl border-8 border-orange-300";
-
-    img.setAttribute("data-aos", "fade-down-right");
-
-    div.appendChild(img)
-  }
-};
-
-window.onload = function() {
-  gallery();
-}
